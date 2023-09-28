691b81306b6d15c8e58bbcfd3ff773b17f0a88ae
latrokles <latrokles@gmail.com>
2023-09-27 18:25:11 -0700

    rename preamble

---
+++ yacl/example.asm
@@ -0,0 +1,4 @@
+	LOAD_CONST 	num 	10
+	LOAD_CONST 	num 	5
+	ADD
+	SYSCALL 	console-print
--- yacl/vm.py
+++ yacl/vm.py
@@ -1,12 +1,31 @@
+import collections
+import dataclasses
 import enum
+
 
 from jacl import Stack
 
 
 class Opcode(enum.IntEnum):
-    LOAD_CONST = 0x00
+    LOAD_CONST   = 0x00
+    SET_GLOBAL   = enum.auto()
+    GET_GLOBAL   = enum.auto()
+
     ...
 
 
+@dataclasses.dataclass
+class MemoryBlock:
+    data: bytearray = dataclasses.field(default_factory=bytearray)
+
+
+
+@dataclasses.dataclass
 class VirtualMachine:
-    pass
+    executing: bool = False
+
+    code: MemoryBlock = dataclasses.field(default_factory=MemoryBlock)
+    heap: MemoryBlock = dataclasses.field(default_factory=MemoryBlock)
+
+    datastack: Stack = dataclasses.field(default_factory=Stack)
+    retainstack: Stack = dataclasses.field(default_factory=Stack)
